fb6b078e69df497f5c0d457f842b40c441ec9199
duhdoesk <dev.edusc@gmail.com>
2022-10-22 16:16:12 -0300

    theme modifications and bingo card ui improvements

--- app/src/main/java/com/example/sorteadordebingo/ui/cartela/CartelaFragment.kt
+++ app/src/main/java/com/example/sorteadordebingo/ui/cartela/CartelaFragment.kt
@@ -4,18 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
@@ -23,7 +19,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,10 +33,11 @@ class CartelaFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         return ComposeView(requireContext()).apply {
             setContent {
                 ProjectTheme {
-                    Surface() {
+                    Surface {
                         SetCartelaFragment()
                     }
                 }
@@ -54,14 +50,12 @@ class CartelaFragment : Fragment() {
     private fun SetCartelaFragment() {
 
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    Brush.verticalGradient(
-                        0f to PrimaryLight,
-                        1000f to PrimaryDark
-                    )
-                ),
+            modifier = Modifier.fillMaxSize().background(
+                Brush.verticalGradient(
+                    if (isSystemInDarkTheme()) 0.0f to Color.Transparent else 0.0f to gradientLighter,
+                    1.0f to Color.Transparent
+                )
+            ),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
@@ -75,10 +69,7 @@ class CartelaFragment : Fragment() {
 //            Botão responsável pelo sorteio da cartela
             Button(
                 onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = SecondaryLight,
-                    contentColor = PrimaryText
-                )
+                elevation = ButtonDefaults.elevation(4.dp)
             ) {
                 Text(text = stringResource(id = R.string.sortear_cartela).uppercase())
             }
@@ -87,47 +78,40 @@ class CartelaFragment : Fragment() {
     }
 
 //    Função responsável pela criação do menu suspenso
+    @OptIn(ExperimentalMaterialApi::class)
     @Composable
-    fun DropdownMenu() {
+    private fun DropdownMenu() {
+
+        val options = listOf("Bichos", "Flores", "Frutas")
         var expanded by remember { mutableStateOf(false) }
-        val items = listOf("Bichos", "Flores")
-        var selectedIndex by remember { mutableStateOf(0) }
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween,
-            modifier = Modifier
-                .width(200.dp)
-                .height(32.dp)
-                .background(gridBackgroud, RoundedCornerShape(8.dp))
-                .border(1.dp, PrimaryText, RoundedCornerShape(8.dp))
-                .clickable(onClick = { expanded = true })
+        var selectedOptionText by remember { mutableStateOf(options[0]) }
+
+        ExposedDropdownMenuBox(
+            expanded = expanded,
+            onExpandedChange = { expanded = !expanded }
         ) {
-            Text(
-                items[selectedIndex],
-                color = PrimaryText,
-                modifier = Modifier
-                    .fillMaxWidth(0.82f)
-                    .padding(start = 12.dp)
+            TextField(
+                readOnly = true,
+                value = selectedOptionText,
+                onValueChange = { },
+                label = { Text(text = "Tema do Bingo") },
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+                },
+                colors = ExposedDropdownMenuDefaults.textFieldColors(backgroundColor = gridBackground)
             )
 
-            Image(
-                painter = painterResource(id = R.drawable.ic_baseline_arrow_drop_down_24),
-                contentDescription = "Dropdown Arrow",
-                Modifier.fillMaxWidth()
-            )
-
-            DropdownMenu(
+            ExposedDropdownMenu(
                 expanded = expanded,
-                onDismissRequest = { expanded = false },
-                modifier = Modifier.width(200.dp)
+                onDismissRequest = { expanded = false }
             ) {
-                items.forEachIndexed { index, option ->
-                    DropdownMenuItem(onClick = {
-                        selectedIndex = index
-                        expanded = false
-                    }) {
-                        Text(text = option)
-                    }
+                options.forEach { selectionOption ->
+                    DropdownMenuItem(
+                        onClick = {
+                            selectedOptionText = selectionOption
+                            expanded = false
+                        }
+                    ) { Text(text = selectionOption) }
                 }
             }
         }
@@ -147,14 +131,14 @@ class CartelaFragment : Fragment() {
                 .padding(vertical = 16.dp)
                 .clip(RoundedCornerShape(12.dp))) {
             repeat(3) { row ->
-                Row() {
+                Row {
                     repeat(3) { column ->
                         Column(
                             modifier = Modifier
                                 .padding(1.dp)
                                 .width(100.dp)
-                                .height(120.dp)
-                                .background(gridBackgroud),
+                                .height(124.dp)
+                                .background(gridBackground),
                             horizontalAlignment = Alignment.CenterHorizontally,
                             verticalArrangement = Arrangement.Center
                         ) {
@@ -169,10 +153,8 @@ class CartelaFragment : Fragment() {
 //                            Nome do Bicho
                             Text(
                                 text = bichos[row][column],
-                                textAlign = TextAlign.Center,
-                                color = PrimaryText
+                                textAlign = TextAlign.Center
                             )
-
                         }
                     }
                 }
--- app/src/main/java/com/example/sorteadordebingo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/sorteadordebingo/ui/theme/Theme.kt
@@ -1,28 +1,26 @@
 package com.example.sorteadordebingo.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
-private val ProjectColorPalette = lightColors(
-    primary = Primary,
-    primaryVariant = PrimaryLight,
-    secondary = Secondary,
-    secondaryVariant = SecondaryLight,
-    background = Surface,
-    onPrimary = PrimaryText,
-    onSecondary = SecondaryText,
-    onBackground = PrimaryText,
-    onSurface = PrimaryText,
-    surface = Surface
+private val LightColorPalette = lightColors(
+    primary = FlowersPrimaryDark,
+    primaryVariant = FlowersPrimary,
+    onPrimary = Color.White,
+    onSurface = Color.White,
+    surface = FlowersPrimary
 )
 
 @Composable
 fun ProjectTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = ProjectColorPalette
+    val colors = if (darkTheme) darkColors() else LightColorPalette
 
     MaterialTheme(
         colors = colors,
